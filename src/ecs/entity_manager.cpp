73d877815abda916cad70a82054545c75c50b327
Isaac Trimble-Pederson <wtpisaac@gmail.com>
2026-03-08 19:33:26 -0700

    fix: postfix for entity id

--- src/ecs/entity_manager.cpp
+++ src/ecs/entity_manager.cpp
@@ -6,6 +6,6 @@ EntityManager::createEntity() {
         // increment the total entities, and return the number correspondent to the total
         // for now I don't want there to be an entity with ID of 0, in case no entity is
         // useful to represent
-        this->totalEntities++
+        ++this->totalEntities
     );
 }
