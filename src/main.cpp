0f6e6bfd7af7c1f7c435fc3993eb007af1bfea23
Isaac Trimble-Pederson <wtpisaac@gmail.com>
2026-03-21 16:49:11 -0700

    refactor: remove ecs stubs

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,12 @@
 #include <quill/LogMacros.h>
 
 #include "core/logger.hpp"
-#include "ecs/entity.hpp"
-#include "ecs/entity_manager.hpp"
 
 int main()
 {
     QUILL_LOG_INFO(
         BIRBS_LOGGER,
         "Hello, World!"
-    );
-    EntityManager manager = EntityManager();
-    Entity entity = manager.createEntity();
-    Entity secondEntity = manager.createEntity();
-    QUILL_LOG_INFO(
-        BIRBS_LOGGER,
-        "I've got {} and {}!",
-        entity,
-        secondEntity
     );
     return 0;
 }
