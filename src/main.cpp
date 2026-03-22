432acbce949e7a193dab706878ad2b399d8febbe
Isaac Trimble-Pederson <wtpisaac@gmail.com>
2026-03-21 17:06:17 -0700

    feat: simple raylib loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,42 @@
+#include <raylib.h>
 #include <quill/LogMacros.h>
 
 #include "core/logger.hpp"
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define APPLICATION_TITLE "birbs"
 
 int main()
 {
     QUILL_LOG_INFO(
         BIRBS_LOGGER,
-        "Hello, World!"
+        "birbs!"
     );
+
+    // Start Raylib rendering
+    InitWindow(
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        APPLICATION_TITLE
+    );
+    SetTargetFPS(60);
+
+    // Run loop
+    while(!WindowShouldClose()) {
+        // Logic
+
+        // Drawing
+        BeginDrawing();
+        ClearBackground(DARKBLUE);
+        EndDrawing();
+    }
+
+    // Close up
+    QUILL_LOG_INFO(
+        BIRBS_LOGGER,
+        "birbs closing!"
+    );
+    CloseWindow();
     return 0;
 }
